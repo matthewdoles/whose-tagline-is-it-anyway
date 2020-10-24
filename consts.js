0f29f7296d501918c2225343a08b9567e6be8ad2
Matthew Doles <matteddied@gmail.com>
2020-10-24 15:23:04 -0400

    Add voice name and product id to consts, format index.js

--- consts.js
+++ consts.js
@@ -1,3 +1,5 @@
 export const MOVIEDB_API_KEY = process.env.MOVIE_DB_API_KEY;
-export const MOVIEDB_LANG = 'en-US';
 export const MOVIEDB_HOST = 'api.themoviedb.org';
+export const MOVIEDB_LANG = 'en-US';
+export const PRODUCT_ID = process.env.GOOD_WORD_HUNTING_PRODUCT_ID;
+export const VOICE_NAME = 'Matthew';
